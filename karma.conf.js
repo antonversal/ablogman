b0164b7e42a6bb815a172e202051e5d313adc0e4
Anton Versal <ant.ver@gmail.com>
2014-05-01 11:58:29 +0300

    setup karma

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
@@ -43,7 +43,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
